7eba3517b1594522036c3d5d6df29cab7d15c25a
Phillip Shannon & alexandra brown <student@epicodus.com>
2015-08-11 10:59:29 -0700

    Create test for spec returning two equal words

--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -3,9 +3,18 @@
 
     class AnagramTest extends PHPUnit_Framework_TestCase
     {
-      function test_anagramFinder_sameWord()
-      {
-        
-      }
+        function test_anagramFinder_sameWord()
+        {
+            //Arrange
+            $test_Anagram = new Anagram;
+            $word = "bread";
+            $list = "bread";
+
+            //Act
+            $result = $test_Anagram->anagramFinder($word, $list);
+
+            //Assert
+            $this->assertEquals("bread", $result);
+        }
     }
 ?>
